ccab39467347df07abe6e1d11b4271572e207ef7
theWarlock <patrickvito2012@gmail.com>
2023-07-28 11:36:34 +0100

    change the text

--- components/tax/tax-body.tsx
+++ components/tax/tax-body.tsx
@@ -8,26 +8,25 @@ const TaxBody = () => {
         <div>
           <p className="capitalize leading-loose mb-6">
             It is your responsibility to report and pay any taxes that may arise
-            from transacting on the Coins Exchange Services, and you acknowledge
-            that Coins Exchange does not provide legal or tax advice relative to
+            from transacting on the VSAexchange Services, and you acknowledge
+            that VSAexchange does not provide legal or tax advice relative to
             these transactions. If you have any doubts about your tax status or
-            obligations when using Coins Exchange Services, or with respect to
-            the Digital Assets held to the credit of your Coins Exchange
-            account, you may wish to seek independent advice. You acknowledge
-            that, when, where and as required by applicable legislation, Coins
-            Exchange shall report information regarding your transactions,
-            transfers, distributions or payments to tax or other public
-            authorities
+            obligations when using VSAexchange Services, or with respect to the
+            Digital Assets held to the credit of your VSAexchange account, you
+            may wish to seek independent advice. You acknowledge that, when,
+            where and as required by applicable legislation, Coins Exchange
+            shall report information regarding your transactions, transfers,
+            distributions or payments to tax or other public authorities
           </p>
           <p className="capitalize leading-loose mb-6">
             Similarly, when, where and as required by applicable law, Coins
             Exchange shall withhold taxes related to your transactions,
             transfers, distributions or payments. Applicable legislation could
-            also prompt Coins Exchange to request you for additional tax
+            also prompt VSAexchange to request you for additional tax
             information, status, certificates or documentation. You acknowledge
             that failure to answer these requests within the timeframe defined,
-            may result in withholding taxes by Coins Exchange , to be remitted
-            to tax authorities as defined by relevant law. You are encouraged to
+            may result in withholding taxes by VSAexchange , to be remitted to
+            tax authorities as defined by relevant law. You are encouraged to
             seek professional and personal tax advice regarding the above and
             before making any digital asset transaction.
           </p>
